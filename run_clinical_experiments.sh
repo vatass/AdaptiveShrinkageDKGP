570efcb1349df5093f3bb7ef9c8ed8eb2e425209
Vasiliki Tassopoulou <tassvasiliki@gmail.com>
2026-03-29 02:16:45 -0400

    clinical experiments

---
+++ run_clinical_experiments.sh
@@ -0,0 +1,11 @@
+# bash script
+
+python progressors_non_progressors_group_differences.py
+python mci_progression_prediction_volumns.py 
+python diagnosis_classification.py 
+python simulated_trial.py 
+
+
+python spare_ad_experiment.py
+python brain_age_gap_analysis.py
+
